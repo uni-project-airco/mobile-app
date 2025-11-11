5d05539c2536382195f63a8cd76a4037e4958a40
yakovenkoka <D00272517@student.dkit.ie>
2025-11-11 18:11:09 +0000

    Added logo and header text to registration screen

--- app/src/main/java/com/example/safeairapp/ui/screens/RegisterScreen.kt
+++ app/src/main/java/com/example/safeairapp/ui/screens/RegisterScreen.kt
@@ -3,19 +3,42 @@ package com.example.safeairapp.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.safeairapp.R
 
 @Composable
 fun RegisterScreen(modifier: Modifier= Modifier){
+    var fullName by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+
+    val gradient = Brush.verticalGradient(
+        colors = listOf(
+            Color(0xFFFFFFFF),
+            Color(0xFFD4D4D4)
+        )
+    )
 
     Box(
         modifier = modifier.fillMaxSize()
@@ -34,6 +57,26 @@ fun RegisterScreen(modifier: Modifier= Modifier){
                 .padding(horizontal = 60.dp, vertical = 40.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(R.drawable.safeair_logo_text_b),
+                contentDescription = "SafeAir logo",
+                modifier = Modifier
+                    .height(90.dp)
+            )
+
+            Spacer(modifier = Modifier.height(52.dp))
+
+            Text(
+                text = "Create Account!",
+//                fontFamily = montserrat,
+//                fontWeight = FontWeight.Medium,
+                fontSize = 34.sp,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
 
         }
     }
