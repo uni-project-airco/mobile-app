6178b46705b2c7a8d63ac7ba04d1ef5515b26dc4
yakovenkoka <D00272517@student.dkit.ie>
2025-11-11 00:07:09 +0000

    Added email and password input fields, sign-in button, and remaining text to LoginScreen

--- app/src/main/java/com/example/safeairapp/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/safeairapp/ui/screens/LoginScreen.kt
@@ -1,21 +1,37 @@
 package com.example.safeairapp.ui.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,6 +41,9 @@ import com.example.safeairapp.R
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
     val monsteratt = FontFamily(
         Font(R.font.montserrat_light, FontWeight.Light),
         Font(R.font.montserrat, FontWeight.Normal),
@@ -32,6 +51,13 @@ fun LoginScreen(modifier: Modifier = Modifier){
         Font(R.font.montserrat_semibold, FontWeight.SemiBold),
         Font(R.font.montserrat_bold, FontWeight.Bold),
         Font(R.font.montserrat_thin, FontWeight.Thin)
+    )
+
+    val gradient = Brush.verticalGradient(
+        colors = listOf(
+            Color(0xFFFFFFFF),
+            Color(0xFFD4D4D4)
+        )
     )
 
     Column(modifier = modifier
@@ -52,7 +78,7 @@ fun LoginScreen(modifier: Modifier = Modifier){
             .padding(horizontal = 60.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(modifier = Modifier.height(24.dp))
+            Spacer(modifier = Modifier.height(12.dp))
 
             Text(
                 text = "Login",
@@ -63,7 +89,107 @@ fun LoginScreen(modifier: Modifier = Modifier){
                 textAlign = TextAlign.Start
             )
 
-            Spacer(modifier = Modifier.height(20.dp))
+            Spacer(modifier = Modifier.height(40.dp))
+
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                placeholder = {
+                    Text(
+                        "Email...",
+                        fontFamily = monsteratt,
+                        fontSize = 18.sp
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(52.dp)
+                    .background(brush = gradient, shape = RoundedCornerShape(15.dp))
+                    .border(1.dp, Color.Black, RoundedCornerShape(15.dp)),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(15.dp),
+                textStyle = TextStyle(
+                    fontFamily = monsteratt,
+                    fontSize = 18.sp
+                )
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                placeholder = {
+                    Text(
+                        "Password...",
+                        fontFamily = monsteratt,
+                        fontSize = 18.sp
+                    )
+                },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(51.dp)
+                    .background(brush = gradient, shape = RoundedCornerShape(15.dp))
+                    .border(1.dp, Color.Black, RoundedCornerShape(15.dp)),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(15.dp),
+                textStyle = TextStyle(
+                    fontFamily = monsteratt,
+                    fontSize = 18.sp
+                )
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = "Forgot Password?",
+                fontFamily = monsteratt,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.End
+            )
+
+            Spacer(modifier = Modifier.height(28.dp))
+
+            Button (
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
+                shape = RoundedCornerShape(50),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(55.dp)
+            ) {
+                Text(
+                    text = "Sign in",
+                    color = Color.White,
+                    fontFamily = monsteratt,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 22.sp
+                )
+            }
+
+            Spacer(modifier = Modifier.height(26.dp))
+
+            Text(
+                text = "Don’t have an account? Sign up",
+                fontFamily = monsteratt,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp
+            )
         }
 
     }
